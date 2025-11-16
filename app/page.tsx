73fbfaf01d03a3567cd42b94a7fe0fff7fcfd228
rigby200602 <tungle3456@gmail.com>
2025-11-16 14:29:29 +0700

    add frame

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between py-16 px-8">
-      <div className="">
+    <main className="flex flex-col items-center justify-between">
+      <div className="py-16 px-8">
         <h1 className="text-[40px] text-[#121216] font-semibold text-center">Travel Smarter, Not Harder</h1> 
         <p className="text-lg text-[#767689] font-normal text-center">
           Make every trip effortless. Tripzy lets you book rides and plan journeys with ease
         </p>
+      </div>
+      <div className="bg-white rounded-2xl h-60 w-[75%] shadow-lg ">
+          
       </div>
     </main>
   );
