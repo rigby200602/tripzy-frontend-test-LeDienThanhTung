c5af5e83dc7b7d3ff06b531d315ca59f35539eec
rigby200602 <tungle3456@gmail.com>
2025-11-16 14:14:59 +0700

    update text color, update globalcss

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <div className="my-16">
-        <h1 className="text-[40px] font-semibold text-center">Travel Smarter, Not Harder</h1> 
-        <p className="text-lg font-normal text-center">
+        <h1 className="text-[40px] text-[#121216] font-semibold text-center">Travel Smarter, Not Harder</h1> 
+        <p className="text-lg text-[#767689] font-normal text-center">
           Make every trip effortless. Tripzy lets you book rides and plan journeys with ease
         </p>
       </div>
