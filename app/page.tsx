bcbe669b17394f82cc43890f30105ced913b2429
rigby200602 <tungle3456@gmail.com>
2025-11-16 14:16:53 +0700

    update padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <div className="my-16">
+      <div className="py-16 px-8">
         <h1 className="text-[40px] text-[#121216] font-semibold text-center">Travel Smarter, Not Harder</h1> 
         <p className="text-lg text-[#767689] font-normal text-center">
           Make every trip effortless. Tripzy lets you book rides and plan journeys with ease
