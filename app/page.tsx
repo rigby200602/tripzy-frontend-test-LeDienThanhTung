e1fbf97809a93ab181ac97a2de57c1f040fff8f0
rigby200602 <tungle3456@gmail.com>
2025-11-16 14:49:16 +0700

    update css

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         </p>
       </div>
       <div className="flex flex-col bg-white rounded-2xl h-60 w-[75%] shadow-lg">
-          <div className="flex rounded-2xl shadow-xl h-[30%]">
+          <div className="flex rounded-2xl shadow-md h-[30%]">
           </div>
       </div>
     </main>
