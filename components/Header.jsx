c1fbf621d94f19e1182d8119c43b2785feb0109c
rigby200602 <tungle3456@gmail.com>
2025-11-16 20:53:48 +0700

    replace logo with header

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MdEmergency } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <div className='text-[#19C0FF] font-bold'>
+      <MdEmergency />
+      <span>Tripzy</span>
+    </div>
+  )
+}
+
+export default Header
