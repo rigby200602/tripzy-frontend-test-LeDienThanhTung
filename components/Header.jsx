ace8cc9a58943a4f41f2546421b3d23800730a5a
rigby200602 <tungle3456@gmail.com>
2025-11-16 21:02:33 +0700

    finish header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import React from 'react'
 import { MdEmergency } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className='text-[#19C0FF] font-bold'>
-      <MdEmergency />
-      <span>Tripzy</span>
+    <div className='flex text-[#19C0FF] text-[28px] font-bold mx-[12.5%] pt-4'>
+      <MdEmergency className='mt-[0.5%]'/>
+      <Link href={'/'}>Tripzy</Link>
     </div>
   )
 }
