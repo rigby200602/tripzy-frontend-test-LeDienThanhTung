b56bf20fb563fccf02626db852c89a32562d5819
rigby200602 <tungle3456@gmail.com>
2025-11-16 21:02:27 +0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between">
-      <div className="py-16 px-8">
-        <h1 className="text-[40px] text-[#121216] font-semibold text-center">Travel Smarter, Not Harder</h1> 
-        <p className="text-lg text-[#767689] font-normal text-center">
-          Make every trip effortless. Tripzy lets you book rides and plan journeys with ease
-        </p>
+    <div>
+      <Header />
+      <div className="flex flex-col items-center justify-between">
+        <div className="py-16 px-8">
+          <h1 className="text-[40px] text-[#121216] font-semibold text-center">
+            Travel Smarter, Not Harder
+          </h1>
+          <p className="text-lg text-[#767689] font-normal text-center">
+            Make every trip effortless. Tripzy lets you book rides and plan
+            journeys with ease
+          </p>
+        </div>
+        <div className="flex flex-col bg-white rounded-2xl h-60 w-[75%] shadow-lg">
+          <div className="flex rounded-2xl shadow-md h-[30%]"></div>
+        </div>
       </div>
-      <div className="flex flex-col bg-white rounded-2xl h-60 w-[75%] shadow-lg">
-          <div className="flex rounded-2xl shadow-md h-[30%]">
-          </div>
-      </div>
-    </main>
+    </div>
   );
 }
