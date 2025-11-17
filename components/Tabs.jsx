794b92dad29fea81fc046ac2e21326309c11ca07
rigby200602 <tungle3456@gmail.com>
2025-11-17 21:51:04 +0700

    upperCase

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -32,7 +32,7 @@ const Tabs = () => {
       <div className="flex rounded-2xl shadow-md h-[30%]">
         <div className="flex w-full cursor-pointer">
           {tabs.map((tab, index) => {
-            // check which icon to use 
+            // Check which icon to use 
             const Component =
               tab.component === "FaBusAlt"
                 ? FaBusAlt
