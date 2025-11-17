423baceafef02714067eccd84043fcc6a9e9cfb6
rigby200602 <tungle3456@gmail.com>
2025-11-17 16:55:04 +0700

    update static page search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,6 +7,11 @@ const Search = () => {
       <Header />
       <div className="flex justify-center items-center mt-6 h-[75%]">
         <div className="container flex flex-col rounded-2xl bg-white w-[75%] shadow-lg h-full">
+          <p className="px-16 mt-10 font-semibold text-2xl">From:</p>
+          <p className="px-16 mt-10 font-semibold text-2xl">To:</p>
+          <p className="px-16 mt-10 font-semibold text-2xl">Departure date:</p>
+          <p className="px-16 mt-10 font-semibold text-2xl">Return date:</p>
+          <p className="px-16 mt-10 font-semibold text-2xl">No. of passenger:</p>
         </div>
       </div>
     </div>
