96b200f937203004c70b86783cec3e55b36bda4b
rigby200602 <tungle3456@gmail.com>
2025-11-17 16:45:00 +0700

    build a form for search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Header from '@/components/Header';
+import React from "react";
+import Header from "@/components/Header";
 
 const Search = () => {
   return (
-    <div>
-        <Header />
+    <div className="w-full h-full">
+      <Header />
+      <div className="flex justify-center items-center mt-6 h-[75%]">
+        <div className="container flex flex-col rounded-2xl bg-white w-[75%] shadow-lg h-full">
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
