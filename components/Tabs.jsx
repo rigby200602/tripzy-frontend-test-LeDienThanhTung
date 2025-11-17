476cfae9ceaed624e6f407cc4050ca9a5538fcf5
rigby200602 <tungle3456@gmail.com>
2025-11-17 21:02:21 +0700

    update layout for tabs

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,29 +1,48 @@
-import path from 'path'
-import React from 'react'
+import React from "react";
+import { FaBusAlt, FaHotel } from "react-icons/fa";
+import { MdFlightTakeoff } from "react-icons/md";
 
 const tabs = [
-    { 
-        title: "Bus & Shuttle",
-        component: "FaBusAlt",
-        path: "react-icons/fa"
-    },
-    {
-        title: "Hotel & Accommodation",
-        component: "FaHotel",
-        path: "react-icons/fa"
-    },
-    {
-        title: "Flight",
-        component: "MdFlightTakeoff",
-        path: "react-icons/md"
-    }
-]
-
+  {
+    title: "Bus & Shuttle",
+    component: "FaBusAlt",
+    backgroundColor: "bg-[#D3F3FF]",
+    textColor: "text-[#19C0FF] ",
+    hoverBg: "hover:bg-[#EBF9FF]"
+  },
+  {
+    title: "Hotel & Accommodation",
+    component: "FaHotel",
+    backgroundColor: "bg-[#E8FBCC]",
+    textColor: "text-[#447A11] ",
+    hoverBg: "hover:bg-[#F4FFEB]"
+  },
+  {
+    title: "Flight",
+    component: "MdFlightTakeoff",
+    backgroundColor: "bg-[#E1EDFE]",
+    textColor: "text-[#5664E1] ",
+    hoverBg: "hover:bg-[#EBF4FF]"
+  },
+];
 
 const Tabs = () => {
   return (
-    <div>Tabs</div>
-  )
-}
+    <div className="flex w-full">
+      {tabs.map((tab, index) => {
+        const Component = tab.component === "FaBusAlt" ? FaBusAlt : 
+        tab.component === "FaHotel" ? FaHotel : MdFlightTakeoff;
+        return (
+          <div key={index} className={`w-full flex m-2 rounded-xl ${tab.hoverBg}`}>
+            <div className={`rounded-full h-12 w-12 m-1 p-3 ${tab.backgroundColor}`}>
+              <Component className={`text-2xl ${tab.textColor}`} />
+            </div>
+            <p className="font-medium my-auto text-center">{tab.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Tabs
+export default Tabs;
