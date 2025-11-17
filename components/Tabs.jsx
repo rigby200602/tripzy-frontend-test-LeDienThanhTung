1e679a21291e890a6d08e45134c644e311ad4871
rigby200602 <tungle3456@gmail.com>
2025-11-17 12:27:34 +0700

    test data

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const item = [
+    { 
+        title: "Bus & Shuttle"
+    },
+    {
+        title: "Hotel & Accommodation"
+    },
+    {
+        title: "Flight"
+    }
+]
+
+
 const Tabs = () => {
   return (
     <div>Tabs</div>
