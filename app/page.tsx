a4926bbeb6239b00043b733b2cccdc02a273200f
rigby200602 <tungle3456@gmail.com>
2025-11-17 21:20:46 +0700

    update tabs component

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,8 @@ export default function Home() {
             journeys with ease
           </p>
         </div>
-        <div className="flex flex-col bg-white rounded-2xl h-60 w-[75%] shadow-lg">
-          <div className="flex rounded-2xl shadow-md h-[30%]">
             <Tabs/>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -8,39 +8,52 @@ const tabs = [
     component: "FaBusAlt",
     backgroundColor: "bg-[#D3F3FF]",
     textColor: "text-[#19C0FF] ",
-    hoverBg: "hover:bg-[#EBF9FF]"
+    hoverBg: "hover:bg-[#EBF9FF]",
   },
   {
     title: "Hotel & Accommodation",
     component: "FaHotel",
     backgroundColor: "bg-[#E8FBCC]",
     textColor: "text-[#447A11] ",
-    hoverBg: "hover:bg-[#F4FFEB]"
+    hoverBg: "hover:bg-[#F4FFEB]",
   },
   {
     title: "Flight",
     component: "MdFlightTakeoff",
     backgroundColor: "bg-[#E1EDFE]",
     textColor: "text-[#5664E1] ",
-    hoverBg: "hover:bg-[#EBF4FF]"
+    hoverBg: "hover:bg-[#EBF4FF]",
   },
 ];
 
 const Tabs = () => {
   return (
-    <div className="flex w-full cursor-pointer">
-      {tabs.map((tab, index) => {
-        const Component = tab.component === "FaBusAlt" ? FaBusAlt : 
-        tab.component === "FaHotel" ? FaHotel : MdFlightTakeoff;
-        return (
-          <div key={index} className={`w-full flex m-2 rounded-xl ${tab.hoverBg}`}>
-            <div className={`rounded-full h-12 w-12 m-1 p-3 ${tab.backgroundColor}`}>
-              <Component className={`text-2xl ${tab.textColor}`} />
-            </div>
-            <p className="font-medium my-auto text-center">{tab.title}</p>
-          </div>
-        );
-      })}
+    <div className="flex flex-col bg-white rounded-2xl h-60 w-[75%] shadow-lg">
+      <div className="flex rounded-2xl shadow-md h-[30%]">
+        <div className="flex w-full cursor-pointer">
+          {tabs.map((tab, index) => {
+            const Component =
+              tab.component === "FaBusAlt"
+                ? FaBusAlt
+                : tab.component === "FaHotel"
+                ? FaHotel
+                : MdFlightTakeoff;
+            return (
+              <div
+                key={index}
+                className={`w-full flex m-2 rounded-xl ${tab.hoverBg}`}
+              >
+                <div
+                  className={`rounded-full h-12 w-12 m-1 p-3 ${tab.backgroundColor}`}
+                >
+                  <Component className={`text-2xl ${tab.textColor}`} />
+                </div>
+                <p className="font-medium my-auto text-center">{tab.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
