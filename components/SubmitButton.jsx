a9337946a10dd0be8b7bf870083298937170e471
rigby200602 <tungle3456@gmail.com>
2025-11-18 17:18:02 +0700

    done static button

--- components/SubmitButton.jsx
+++ components/SubmitButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 const SubmitButton = () => {
   return (
-    <div>SubmitButton</div>
-  )
-}
+    <Link href={'/search'}
+    className="flex justify-center items-center 
+    bg-[#19C0FF] rounded-4xl h-12 w-60 cursor-pointer">
+      <span>
+        <CiSearch className="mx-1 text-white" size={24}/>
+      </span>
+      <p className=" text-white font-bold cursor-pointer">Search</p>
+    </Link>
+  );
+};
 
-export default SubmitButton
+export default SubmitButton;
